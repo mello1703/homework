71f51eb8aa7d835a4bef1c0c3f3316d93f917bc5
ilaavrumov <iliya200022@gmail.com>
2024-12-11 01:10:49 +0300

    fixes

--- src/widget.py
+++ src/widget.py
@@ -1,24 +1,20 @@
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(nums: str) -> str | None:
+def mask_account_card(nums: str) -> str :
     """ Функция, маскирующая счет и карту. """
-    if "счет" in nums:
-        return get_mask_account(nums)
+    if "Счет" in nums:
+        return f"Счет {get_mask_account(nums)}"
     else:
         cards = get_mask_card_number(nums[-16:])
         new_card = nums.replace(nums[-16:], cards)
         return new_card
 
-
 print(mask_account_card("Maestro 1596837868705199"))
 print(mask_account_card("Счет 64686473678894779589"))
 print(mask_account_card("MasterCard 7158300734726758"))
 print(mask_account_card("Счет 35383033474447895560"))
-print(mask_account_card("Visa Classic 6831982476737658"))
-print(mask_account_card("Visa Platinum 8990922113665229"))
-print(mask_account_card("Visa Gold 5999414228426353"))
-print(mask_account_card("Счет 73654108430135874305"))
+
 
 
 def get_new_data(old_data: str) -> str:
