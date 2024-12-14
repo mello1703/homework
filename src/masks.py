1749d292eda35dd504ae26ad37d58f7e7c48f2c7
ilaavrumov <iliya200022@gmail.com>
2024-12-14 11:11:46 +0300

    masks.py fixes

--- src/masks.py
+++ src/masks.py
@@ -5,7 +5,7 @@
 def get_mask_card_number(card_number: str) -> str:
     """Функция,  которая шифрует номер карты"""
 
-    return f"{card_number[:4]}{card_number[4:6]}** **** {card_number[12:]}"
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
 
 
 def get_mask_account(account_number: str) -> str:
