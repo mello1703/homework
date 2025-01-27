c8f8effae9c5f24e21969e909dbda718e629c1e0
ilaavrumov <iliya200022@gmail.com>
2025-01-27 13:31:04 +0300

    Добавление логеров для модулей: masks.py utils.py

--- src/masks.py
+++ src/masks.py
@@ -1,20 +1,37 @@
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s: %(filename)s: %(funcName)s %(lineno)s: %(asctime)s - %(message)s",
+    filename="../logs/masks_log.log",
+    filemode="w",
+    )
+card_number_logger = logging.getLogger()
+mask_account_logger = logging.getLogger()
+
 # account_number = input()
 # card_number_str = input()
 
 
 def get_mask_card_number(card_number: str) -> str:
     """Функция,  которая шифрует номер карты"""
+    card_number_logger.info("Создаю маску номера карты")
     if len(card_number) == 16 and card_number.isdigit():
+        card_number_logger.error("Неправильный номер карты")
         return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+    card_number_logger.info("Маска номера карты создана")
     return "некорректный ввод"
 
 
 def get_mask_account(account_number: str) -> str:
     """Функция для маскировки номера счета"""
+    mask_account_logger.info("Создаю маску номера счета")
     if len(account_number) == 20 and account_number.isdigit():
+        mask_account_logger.error("Неправильный номер счета")
         return f"**{account_number[-4:]}"
+    mask_account_logger.info("Маска номера счета создана")
     return "некорректный ввод"
 
-#
-# print(get_mask_card_number("7000792289606361"))
-# print(get_mask_account("73654108430135874305"))
+
+print(get_mask_card_number("7000792289606361"))
+print(get_mask_account("73654108430135874305"))
--- src/utils.py
+++ src/utils.py
@@ -4,19 +4,35 @@ from typing import Any
 
 from src.external_api import currency_conversion
 
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(levelname)s: %(filename)s: %(funcName)s %(lineno)s: %(asctime)s - %(message)s",
+    filename="../logs/utils_log.log",
+    filemode="w",
+    )
+financial_transactions_logger = logging.getLogger()
+transaction_amount_logger = logging.getLogger()
+
 
 def financial_transactions(path: str) -> list:
     """Функция принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях."""
     try:
+        financial_transactions_logger.info("Открываю файл с транзакциями")
         with open(path, encoding="utf-8") as financial_file:
             try:
                 transactions = json.load(financial_file)
             except JSONDecodeError:
+                financial_transactions_logger.error("Ошибка файла с транзакциями")
                 return []
         if not isinstance(transactions, list):
+            financial_transactions_logger.error("Список транзакций пуст")
             return []
+        financial_transactions_logger.info("Создан список словарей с данными о финансовых транзакциях")
         return transactions
     except FileNotFoundError:
+        financial_transactions_logger.error("Файл с транзакциями не найден")
         return []
 
 
@@ -24,6 +40,8 @@ def transaction_amount(trans: dict, currency: str = "RUB") -> Any:
     """Функция принимает на вход транзакцию и возвращает сумму транзакции в рублях"""
     if trans["operationAmount"]["currency"]["code"] == currency:
         amount = trans["operationAmount"]["amount"]
+        transaction_amount_logger.info("Код валюты в транзакции RUB")
     else:
         amount = currency_conversion(trans)
+        transaction_amount_logger.info("Код валюты транзакции не RUB, произведена конвертация")
     return amount
